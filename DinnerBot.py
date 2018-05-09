7cf5d0734e56947d512cbdbf9657dadd6ef94d4f
Don Dasalla <dmmdasalla@addu.edu.ph>
2018-05-09 10:22:53 +0800

    Final Update
    I am no longer working on this. The image detection service has stopped working

--- DinnerBot.py
+++ DinnerBot.py
@@ -14,91 +14,38 @@ uri_base = 'westcentralus.api.cognitive.microsoft.com'
 
 @client.event
 async def on_ready():
-    e_msg = discord.Embed().add_field(name="Changes:",
-                                    value='Added stat tracking from Master PUBG. Use "w!stats *handle*" to see a '
-                                          "player's stats", inline=False)
-    for server in client.servers:
-        channels = server.channels
-        ch = discord.utils.get(channels, name='general', type=discord.ChannelType.text)
-        print (ch)
-        ch_id = client.get_channel(ch)
-        print (ch_id)
-        await client.send_message(ch, "WaiterBot has been updated to v0.5!", embed=e_msg)
     print ("Waiter Bot updated!")
-    print ("Waiter Bot v0.5, ready to serve!")
-    return await client.change_presence(game=discord.Game(name='Testing2'))
+    print ("Waiter Bot v0.7, ready to serve!")
+    return await client.change_presence(game=discord.Game(name='w!stats'))
 
 @client.event
 async def on_message(message):
     try:
-        if message.content.lower().startswith("w!stats"):
-            msg = stat_tracker.get_stats(message.content)
-            await client.send_message(message.channel, msg[1], embed=msg[0])
-            return
-        elif('.png') in str(message.attachments[0]) or ('.jpg') in str(message.attachments[0]):
-            img = json.loads(json.dumps(message.attachments[0]))
-            img_url = img['url']
-            img_get = {'url': img_url}
-            img_get2 = json.dumps(img_get)
-            print (img_get2)
-
-            headers = {
-                'Content-Type': 'application/json',
-                'Ocp-Apim-Subscription-Key': subscription_key,
-            }
-            params = urllib.parse.urlencode({
-                'language': 'unk',
-                'detectOrientation ': 'true',
-            })
-            conn = http.client.HTTPSConnection(uri_base)
-            conn.request("POST", "/vision/v1.0/ocr?%s" % params, img_get2, headers)
-
-            response = conn.getresponse()
-            data = response.read()
-            parsed = json.loads(data)
-
-            print("Response:")
-            print(json.dumps(parsed, sort_keys=True, indent=2))
-            conn.close()
-
-            img_txtinfo = str(parsed['regions'])
-            print (img_txtinfo)
-            win_txt = ['WINNER', 'CHICKEN', 'DINNER', 'EXIT', 'TO', 'LOBBY']
-            def checkwin_img():
-                chk = 0
-                for i in win_txt:
-                    if i in img_txtinfo:
-                        chk += 1
-                return chk
-            print (checkwin_img())
-            if checkwin_img() == 6:
-                chicken_food = ['roasted chicken', 'fried chicken', 'chicken wings', 'chicken nuggets']
-                buildargs = {
-                    'serviceName': 'customsearch',
-                    'version': 'v1',
-                    'developerKey': 'AIzaSyCErN7v0GStDFah5P8rNyj4LvIQj0C8rMY'
-                }
-                cseargs = {
-                    'q': random.choice(chicken_food),
-                    'cx': '018188259780999380729:dscyf9jt_vm',
-                    'searchType': 'image',
-                    'imgType': 'photo',
-                    'num': 10
-                }
-                results = search_google.api.results(buildargs, cseargs)
-                links = results.links
-                img_link = random.choice(links)
-                e_img = discord.Embed()
-                e_img = e_img.set_image(url = img_link)
-                print(links)
-                print(img_link)
-                type(img_link)
-                await client.send_message(message.channel, ":chicken: Congratulations on ranking first, " +
-                        message.author.mention + "! Here's your chicken dinner: ", embed = e_img)
+        if not message.attachments:
+            if message.content.lower().startswith("w!stats"):
+                msg = stat_tracker.get_stats(message.content)
+                if len(msg) == 4:
+                    await client.send_message(message.channel, msg[3], embed=msg[0])
+                    await client.send_message(message.channel, embed=msg[1])
+                    await client.send_message(message.channel, embed=msg[2])
+                elif len(msg) == 3:
+                    await client.send_message(message.channel, msg[2], embed=msg[0])
+                    await client.send_message(message.channel, embed=msg[1])
+                else:
+                    await client.send_message(message.channel, msg[1], embed=msg[0])
+                return
+            elif message.content.lower().startswith("w!cmds"):
+                e_msg = discord.Embed()
+                e_msg.add_field(name="w!stats [region] [handle] ('recent'): ",
+                                value="See a PUBG player's (recent) stats from a specifc region.\n\n"
+                                      "Region codes:\n"
+                                      "-SEA: Southeast Asia\n-AS: Asia\n-NA: North America\n"
+                                      "-EU: Europe\n-OC: Oceania.",
+                                inline=True)
+                return await client.send_message(message.channel, "List of WaiterBot commands: ",embed=e_msg)
             return
     except Exception as e:
-        print (e)
         traceback.print_exc()
     return
 
-client.run('NDA2ODA3NzY5MjI5MDMzNTAz.DU4rFQ.Y__6zlIORmqqiKHO01CXOApPgBU')
+client.run('NDA1NzEwNzY4OTc4NTI2MjA4.DUoXbA.YKgLUl9cqfgB5VqoAkAbCJKaRQI')
